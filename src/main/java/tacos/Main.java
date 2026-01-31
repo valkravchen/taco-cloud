d207ea010fc172d0518c9b66da7b5e5fda7588de
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-31 16:38:54 +0300

    3.1.2 Использование JdbcTemplate

---
+++ src/main/java/tacos/Main.java
@@ -0,0 +1,17 @@
+package tacos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        double discount = scanner.nextDouble();
+
+        System.out.println("С днем рождения, " + name + "! Сегодня Вам " + age + "!");
+        System.out.println("По этому поводу дарим Вам скидку " + discount + "%");
+        System.out.println("на весь ассортимент нашего сайта!");
+    }
+}
+
