6fb0f3e784816c9fadbc26e69f1ac3db85f86b5e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-03 19:46:49 +0300

    3.1.2 Использование JdbcTemplate

--- src/main/java/tacos/Main.java
+++ src/main/java/tacos/Main.java
@@ -8,7 +8,6 @@ public class Main {
         String name = scanner.nextLine();
         int age = scanner.nextInt();
         double discount = scanner.nextDouble();
-
         System.out.println("С днем рождения, " + name + "! Сегодня Вам " + age + "!");
         System.out.println("По этому поводу дарим Вам скидку " + discount + "%");
         System.out.println("на весь ассортимент нашего сайта!");
